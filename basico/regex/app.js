5399a90daec928a5147013742fad247c7c4c9f16
leoportogtr86 <leoporto_studio@hotmail.com>
2020-07-14 13:52:03 -0300

    small update

--- basico/regex/app.js
+++ basico/regex/app.js
@@ -1,5 +1,28 @@
+//replace + regex
+
 let frase1 = 'The book is on the table.'
 
 let frase2 = frase1.replace(/ /g, '-')
 
+frase2 = frase2.toLocaleLowerCase()
+
 console.log(frase2)
+
+let frase3 = 'João comprou pão francês, açúcar e feijão.'
+
+
+function modificar(string) {
+
+    let string_modificada
+
+    string_modificada = string.replace(/ /g, '-')
+    string_modificada = string_modificada.toLocaleLowerCase()
+    
+
+    return string_modificada
+    
+}
+
+console.log(modificar(frase3))
+
+
