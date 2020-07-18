8ef8aaeeb619ce923444187a38a2573b37bdc82b
leoporto <leoporto_studio@hotmail.com>
2020-07-18 06:54:10 -0300

    web scrapping pra obter e tratar os dados com regex e retornar um objeto com os dados do pokemon

---
+++ backend/ajax/pokemon.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+
+
+
+axios.get('https://www.pokemon.com/br/pokedex/bulbasaur').then((res)=>{
+
+    const $ = cheerio.load(res.data)
+
+    let pokemon = {
+
+        name: $('.pokedex-pokemon-pagination-title div').text().replace(/\n | |Nº|\d/gi, ''),
+        description: $('.version-descriptions p').text().replace(/\n|  /gi, ''),
+        type: $('.dtm-type').text().replace(/\n|\t|type| /gi, ''),
+        evolutions: $('.evolution-profile h3').text().replace(/\n|  |Nº|\d/gi, '').split(' ')
+    }
+
+
+    console.log(pokemon)
+})
