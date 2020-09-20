7d676c0317d93533e4536c67727e815ed7811014
Leo Porto <leoporto_studio@hotmail.com>
2020-09-20 13:27:15 -0300

    exibindo um simples alerta

---
+++ frontend/js/alertas.js
@@ -0,0 +1,14 @@
+//artigo referencia
+//https://www.devmedia.com.br/alert-em-javascript/37208
+
+let btn = document.getElementById('btn')
+
+
+//um simples alerta, caixa de terxto com uma mensagem.
+
+
+btn.onclick = function() {
+
+    alert('Olá, eu sou um simples alerta!')
+
+}
