d952d2d55815b82b74c56bb21b57e333a282b90a
Leo Porto <leoporto_studio@hotmail.com>
2020-10-01 08:44:07 -0300

    criando um objeto ferrari que herda de Carro

--- basico/heranca/app.js
+++ basico/heranca/app.js
@@ -10,3 +10,22 @@ c1.setVel_max(180)
 
 console.log(c1.toString())
 console.log(c1.__proto__)
+
+
+//objeto que herda de Carro
+
+let ferrari = {
+
+    __proto__: c1
+
+}
+
+
+ferrari.setMarca('ferrari')
+ferrari.setModelo('f40')
+ferrari.setPreco(789000)
+ferrari.setCor('vermelha')
+ferrari.setVel_max(340)
+console.log('--------------------------------------------')
+
+console.log(ferrari.toString());
