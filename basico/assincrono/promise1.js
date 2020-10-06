5ee36b089c2b88f74870fe640bb06c009b564877
Leo Porto <leonardoreisporto@gmail.com>
2020-10-06 07:35:35 -0300

    chamando diversos then encadeados

--- basico/assincrono/promise1.js
+++ basico/assincrono/promise1.js
@@ -2,10 +2,26 @@ let p = new Promise((resolve, reject) => {
 
     resolve('resolvida')
 
-
 })
 
-p.then((res) => {
+p
+    .then((res) => {
 
-    console.log(res)
-})
+        console.log(res)
+    })
+    .then((res) => {
+
+        console.log('segundo then')
+    })
+    .then((res) => {
+
+        console.log('terceiro then')
+    })
+    .then((res) => {
+
+        console.log('quarto then')
+    })
+    .then((res) => {
+
+        console.log('quinto then')
+    })
