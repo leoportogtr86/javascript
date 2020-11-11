cfd610bad5b1f008ed85565b347689602ace7777
leoportogtr86 <leoporto_studio@hotmail.com>
2020-11-11 13:03:28 -0300

    operador destructuring

---
+++ basico/objetos/operador_destructuring.js
@@ -0,0 +1,22 @@
+let c = {
+
+    tonica: 'C',
+    segunda_menor: 'Db',
+    segunda_maior: 'D',
+    terca_menor: 'Eb',
+    terca_maior: 'E',
+    quarta_justa: 'F',
+    quarta_aumentada: 'F#',
+    quinta_justa: 'G',
+    quinta_aumentada: 'G#',
+    sexta_maior: 'A',
+    sexta_menor: 'Ab',
+    sexta_aumentada: 'A#',
+    setima_menor: 'Bb',
+    setima_maior: 'B'
+}
+
+
+let { tonica, terca_maior, quinta_justa } = c
+
+console.log(tonica, terca_maior, quinta_justa)
