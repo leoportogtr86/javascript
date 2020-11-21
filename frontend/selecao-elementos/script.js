aa2924a6a7aafca6d6ec16dc9fab026a2efa3dfb
leoportogtr86 <leoporto_studio@hotmail.com>
2020-11-21 14:12:23 -0300

    selecionando elemento via tagName

--- frontend/selecao-elementos/script.js
+++ frontend/selecao-elementos/script.js
@@ -1,6 +1,13 @@
 let titulo = document.getElementById('titulo')
+let paragrafos = document.getElementsByTagName('p')
 
+//paragrafos não é um array e sim um HTML collection, para manipulacao
+//dele como array, precisamos transformá-lo em array
 
+Array.from(paragrafos).forEach((e) => {
+
+    console.log(e.innerHTML)
+})
 
 titulo.onmouseover = function () {
 
