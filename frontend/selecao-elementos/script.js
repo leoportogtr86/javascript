c22b9c68464429fc7fd53f04c7f0df5ee93d6b9f
leoportogtr86 <leoporto_studio@hotmail.com>
2020-11-21 14:22:51 -0300

    selecionando elemento via className

--- frontend/selecao-elementos/script.js
+++ frontend/selecao-elementos/script.js
@@ -1,5 +1,8 @@
 let titulo = document.getElementById('titulo')
 let paragrafos = document.getElementsByTagName('p')
+let par = document.getElementsByClassName('par')
+let impar = document.getElementsByClassName('impar')
+
 
 //paragrafos não é um array e sim um HTML collection, para manipulacao
 //dele como array, precisamos transformá-lo em array
@@ -14,3 +17,17 @@ titulo.onmouseover = function () {
     titulo.classList.add('vermelho-centro')
 
 }
+
+Array.from(par).forEach((e) => {
+
+    e.style.color = 'blue'
+    e.style.fontWeight = 'bold'
+})
+
+
+Array.from(impar).forEach((e) => {
+
+    e.style.color = 'red'
+    e.style.fontWeight = 'bold'
+    e.style.fontSize = '40px'
+})
