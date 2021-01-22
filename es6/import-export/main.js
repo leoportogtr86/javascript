c986fb41ba8669a6c02068130130d9fb4bc2a7f4
leoportogtr86 <leoporto_studio@hotmail.com>
2021-01-22 10:42:17 -0300

    usando import e export do es6 - classe Carro

--- es6/import-export/main.js
+++ es6/import-export/main.js
@@ -1,6 +1,13 @@
 import Cliente from './models/Cliente.js'
+import Carro from './models/Carro.js'
 
 
 let c1 = new Cliente('Zeca', 45)
 
 console.log(c1.falar())
+
+
+let carro1 = new Carro('land rover', 'range rover evoque', 345000)
+
+console.log(carro1)
+console.log(carro1.andar())
---
+++ es6/import-export/models/Carro.js
@@ -0,0 +1,13 @@
+export default function Carro(marca, modelo, preco) {
+
+
+    this.marca = marca
+    this.modelo = modelo
+    this.preco
+
+    this.andar = () => {
+
+        return 'Vruuummm!!!'
+    }
+
+}
