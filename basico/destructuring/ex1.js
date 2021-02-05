142d759603f644da4ebcd5ddf12c3038c0e5604c
leoportogtr86 <leoporto_studio@hotmail.com>
2021-02-05 08:56:34 -0300

    operador destrucuturing 1 - criando objeto e extraindo seus dados

---
+++ basico/destructuring/ex1.js
@@ -0,0 +1,15 @@
+const funcionario = {
+
+    nome: "Leonardo Porto",
+    empresa: "WiseUp",
+    funcao: "desenvolvedor frontend react js",
+    salario: 8000,
+    tecnologias: ['node js', 'react js', 'next js', 'html', 'css', 'mongodb', 'express', 'grapghql'],
+    dataInicio: '01/04/2021'
+}
+
+
+const { nome, empresa, funcao, dataInicio, tecnologias, salario } = funcionario
+
+
+console.log(nome)
