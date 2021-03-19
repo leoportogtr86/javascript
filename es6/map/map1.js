cdd2d5199dccb6b5f671f22c405dfa3ceb4c0518
leoportogtr86 <leoporto_studio@hotmail.com>
2021-03-19 09:26:10 -0300

    estrutura map - setando e recuperando os dados

--- es6/map/map1.js
+++ es6/map/map1.js
@@ -1,6 +1,34 @@
+//aceita qualquer estrutura (tipos primitivos, objetos, funções etc como chave)
+
 let mapa = new Map()
 
 
-console.log(mapa)
+let string = "Apenas uma string"
+let array = ["java", "js", "ts", "c", "c++"]
+let objeto = {
+
+    nome: "cadeira",
+    preco: 49.90
+}
+let funcao = () => {
+
+    return "Oi, sou uma função!!"
+}
+
+
+//setando as chaves e valores
+
+mapa.set(string, "Uma string")
+mapa.set(array, "um array")
+mapa.set(objeto, "um objeto")
+mapa.set(funcao, "uma função")
+
+
+
+//recuperando os valores a partir de chaves
+console.log(mapa.get(string))
+console.log(mapa.get(array))
+console.log(mapa.get(objeto))
+console.log(mapa.get(funcao))
 
 
