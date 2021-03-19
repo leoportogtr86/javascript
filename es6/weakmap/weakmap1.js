101c4f44806d609df160229ba4640b94958eb9fe
leoportogtr86 <leoporto_studio@hotmail.com>
2021-03-19 09:50:21 -0300

    estrutura weakmap

--- es6/weakmap/weakmap1.js
+++ es6/weakmap/weakmap1.js
@@ -1,5 +1,21 @@
 //estrutura onde as chaves só podem ser objetos
 
+let primeiro = document.getElementById("primeiro")
+let segundo = document.getElementById("segundo")
+let terceiro = document.getElementById("terceiro")
 
+
+
+let weakmap = new WeakMap()
+
+weakmap.set(primeiro, "primeiro elemento")
+weakmap.set(segundo, "segundo elemento")
+weakmap.set(terceiro, "terceiro elemento")
+
+
+
+console.log(weakmap)
+
+weakmap.has(primeiro)
 
 
