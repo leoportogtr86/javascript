a876e478eb9e44cdee011f775ae063cb92a0b6dd
leoportogtr86 <leoporto_studio@hotmail.com>
2021-03-24 16:40:04 -0300

    classes em js

---
+++ basico/classes/Musica.js
@@ -0,0 +1,17 @@
+class Musica {
+
+    constructor(titulo, autor) {
+
+        this.titulo = titulo
+        this.autor = autor
+
+    }
+}
+
+
+let m1 = new Musica("Que País é Esse?", "Renato Russo")
+
+console.log(m1)
+
+console.log(typeof m1)
+console.log(typeof Musica)
---
+++ basico/heranca/entities/Filme.js
@@ -0,0 +1,12 @@
+function Filme(nome, anoLancamento, autor) {
+
+    this.nome = nome
+    this.anoLancamento = anoLancamento
+    this.autor = autor
+
+}
+
+
+let filme1 = new Filme("A Voz Suprema do Blues", 2020, "George C. Wolfe")
+
+console.log(filme1)
