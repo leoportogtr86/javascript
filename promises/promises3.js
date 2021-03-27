9241504a419e0443e0e90744dd5063f6be7c44fe
leoportogtr86 <leoporto_studio@hotmail.com>
2021-03-27 09:01:19 -0300

    mais uma promise

---
+++ promises/promises3.js
@@ -0,0 +1,23 @@
+let promessa = new Promise((resolve, reject) => {
+
+    let soma = 10 + 5
+    if (soma > 20) {
+
+        resolve(soma)
+
+    } else {
+
+        reject("Deu bronca!!")
+    }
+
+})
+
+
+
+
+promessa
+    .then(res => console.log(res))
+    .catch((err) => {
+
+        console.log(err)
+    })
